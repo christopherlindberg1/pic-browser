cd8093e323e147887a94720d321fd3af5fb2ec1a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-02-21 17:10:40 +0100

    Minor fix

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID 6n4GMVE8dOtwx7muMVKT5w0WwqKOZIQ0GVPwm1N1ejE"
+    Authorization: process.env.REACT_APP_API_KEY
   }
 });
